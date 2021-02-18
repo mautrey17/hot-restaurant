3e3a0fc498392a1e2d08ffebc0aee0b56eb59df3
Matt Autrey <mautrey17@gmail.com>
2021-02-17 19:44:21 -0500

    server listens

--- server.js
+++ server.js
@@ -7,3 +7,8 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+
+//have express server start to listen
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
