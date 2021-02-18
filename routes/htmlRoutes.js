f36b27677499b393e350059ad9a91b08cea16c2e
Christian Wittorp <cwwittor@ncsu.edu>
2021-02-17 20:05:04 -0500

    adding to html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,21 +8,21 @@ module.exports = function(app) {
     //this is the main page
     app.get("*", function(res) {
 
-        res.sendFile(path.join(__dirname, "../public/home.html"));
+        res.sendFile(path.join(__dirname, "../home.html"));
 
     });
 
     //user visits the reserve page
     app.get("/reserve", function(res) {
 
-        res.sendFile(path.join(__dirname, "../public/reserve.html"));
+        res.sendFile(path.join(__dirname, "../reserve.html"));
 
     });
 
     //user visits the table page
     app.get("/tables", function(res) {
 
-        res.sendFile(path.join(__dirname, "../public/tables.html"));
+        res.sendFile(path.join(__dirname, "../tables.html"));
 
     });
 
